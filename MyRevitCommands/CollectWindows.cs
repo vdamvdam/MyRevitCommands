873f4cf3207b376fda8bce9ab7c187c507e96f3c
vdamvdam <76465652+vdamvdam@users.noreply.github.com>
2021-07-05 16:09:11 -0700

    Create CollectWindows.cs

---
+++ MyRevitCommands/CollectWindows.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class CollectWindows : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get Document
+            Document doc = uidoc.Document;
+
+            //Create Filtered Element Collector
+
+            //Create Filter
+
+            //Apply Filter
+
+            return Result.Succeeded;
+        }
+    }
+}
