3f5f4fabc570accbe550478324ed52cd032a6872
vdamvdam <76465652+vdamvdam@users.noreply.github.com>
2021-07-05 21:45:25 -0700

    Update SetParameter.cs

--- MyRevitCommands/SetParameter.cs
+++ MyRevitCommands/SetParameter.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.Attributes;
+using System;
 
 namespace MyRevitCommands
 {
@@ -44,7 +40,6 @@ namespace MyRevitCommands
 
                         trans.Commit();
                     }
-
                 }
 
                 return Result.Succeeded;
@@ -54,7 +49,6 @@ namespace MyRevitCommands
                 message = e.Message;
                 return Result.Failed;
             }
-
         }
     }
 }
