dc43f74fbb426da4a7997c80ba971521061ff8be
vdamvdam <76465652+vdamvdam@users.noreply.github.com>
2021-07-05 23:30:10 -0700

    Create PlaceLoopElements.cs

---
+++ MyRevitCommands/Commands/PlaceLoopElements.cs
@@ -0,0 +1,71 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class PlaceLoopElement : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get Document
+            Document doc = uidoc.Document;
+
+            //Create Points
+            XYZ p1 = new XYZ(-10, -10, 0);
+            XYZ p2 = new XYZ(10, -10, 0);
+            XYZ p3 = new XYZ(15, 0, 0);
+            XYZ p4 = new XYZ(10, 10, 0);
+            XYZ p5 = new XYZ(-10, 10, 0);
+
+            //Create Curves
+            List<Curve> curves = new List<Curve>();
+            Line l1 = Line.CreateBound(p1, p2);
+            Arc l2 = Arc.Create(p2, p4, p3);
+            Line l3 = Line.CreateBound(p4, p5);
+            Line l4 = Line.CreateBound(p5, p1);
+
+            curves.Add(l1);
+            curves.Add(l2);
+            curves.Add(l3);
+            curves.Add(l4);
+
+            //Create Curve Loop
+            CurveLoop crvLoop = CurveLoop.Create(curves);
+            double offset = 135;//units are in feet, need a way to convert
+            CurveLoop offsetcrv = CurveLoop.CreateViaOffset(crvLoop, offset, new XYZ(0, 0, 1));
+
+            CurveArray cArray = new CurveArray();
+
+            foreach (Curve c in offsetcrv)
+            {
+                cArray.Append(c);//adds curve to curve array
+            }
+
+            try
+            {
+                using (Transaction trans = new Transaction(doc, "Place Family"))
+                {
+                    _ = trans.Start();
+
+                    _ = doc.Create.NewFloor(cArray, false);
+
+                    _ = trans.Commit();
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
