8461c984baf6e01c45732532f354074ac7fd8060
vdamvdam <76465652+vdamvdam@users.noreply.github.com>
2021-07-05 21:44:32 -0700

    Create GetElementId - Copy.cs

---
+++ MyRevitCommands/GetElementId - Copy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class GetElementId : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get Document
+            Document doc = uidoc.Document;
+            try
+            {
+                //Pick Object
+                Reference pickedObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                //Retrieve Element
+                ElementId eleID = pickedObj.ElementId;
+
+                Element ele = doc.GetElement(eleID);
+
+                //Get Element Type
+                ElementId eTypeId = ele.GetTypeId();
+                ElementType eType = doc.GetElement(eTypeId) as ElementType;
+
+           
+                //Display Element ID
+                if (pickedObj != null)
+                {
+                    TaskDialog.Show("Element Classification", eleID.ToString() + Environment.NewLine
+                        + "Category: " + ele.Category.Name + Environment.NewLine
+                        + "Instance: " + ele.Name + Environment.NewLine
+                        + "Symbol: " + eType.Name + Environment.NewLine
+                        + "Family: " + eType.FamilyName); 
+                }
+                return Result.Succeeded;
+            }
+
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+        }
+          
+    }
+}
